b803c68141132047489da953642c6c19a34b76e5
Raducu Vlad-Rares <vlad101vlad@gmail.com>
2021-10-15 22:53:50 +0300

    Server initial commit

---
+++ Server/funfest/src/main/java/en/ubb/info/funfest/FunfestApplication.java
@@ -0,0 +1,13 @@
+package en.ubb.info.funfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunfestApplication.class, args);
+	}
+
+}
---
+++ Server/funfest/src/test/java/en/ubb/info/funfest/FunfestApplicationTests.java
@@ -0,0 +1,13 @@
+package en.ubb.info.funfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunfestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
