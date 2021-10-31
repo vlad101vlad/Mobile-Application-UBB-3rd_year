92572b852544c7c2d60df7c4b66d49f2455ba84a
Raducu Vlad-Rares <vlad101vlad@gmail.com>
2021-10-31 18:33:43 +0200

    Initial commit backend structure working

---
+++ Server/funfest-application/src/main/java/main/FunfestMainApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+        "ro.ubb.cluj.controller"
+})
+public class FunfestMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FunfestMainApplication.class);
+    }
+}
