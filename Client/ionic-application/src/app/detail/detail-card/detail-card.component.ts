d854118f506bab8778d97f4da3e5ca3a0e7e4b05
Raducu Vlad-Rares <vlad101vlad@gmail.com>
2021-11-01 10:24:24 +0200

    Fixed add contest content and improved details data

--- Client/ionic-application/src/app/detail/detail-card/detail-card.component.ts
+++ Client/ionic-application/src/app/detail/detail-card/detail-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Contest} from "../../shared/contest";
 
 @Component({
   selector: 'funfest-detail-card',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-card.component.scss'],
 })
 export class DetailCardComponent implements OnInit {
+  @Input() contest: Contest;
 
   constructor() { }
 
--- Client/ionic-application/src/app/shared/contest.ts
+++ Client/ionic-application/src/app/shared/contest.ts
@@ -1,5 +1,11 @@
 export class Contest{
-  id: number;
-  title: string;
-  description: string;
+  public id: number;
+  public title: string;
+  public description: string;
+
+  constructor() {
+    this.id = -1;
+    this.title = '';
+    this.description = '';
+  }
 }
