7ee82984cd7b143882b1ff5a5ca8091854ead6b8
Raducu Vlad-Rares <vlad101vlad@gmail.com>
2021-11-13 18:48:51 +0200

    Fixed contest auto generation on primary key

--- Server/funfest-backend/src/main/java/ro/ubb/cluj/domain/ContestJPA.java
+++ Server/funfest-backend/src/main/java/ro/ubb/cluj/domain/ContestJPA.java
@@ -1,14 +1,12 @@
 package ro.ubb.cluj.domain;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Contest")
 public class ContestJPA {
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long id;
 
